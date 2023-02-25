5b08d258a7e4c825b00d09c0a56d57c3a75e9297
HK-RazieL <hkraziel@gmail.com>
2023-02-26 01:55:30 +0200

    Fixed major bug when checking invalid ID

--- modules/convertToJson.js
+++ modules/convertToJson.js
@@ -3,16 +3,18 @@ import xml2js from "xml2js";
 
 export function convertToJSON(id, xmlFilePath, jsonFilePath) {
   try {
-    let fileData = fs.readFileSync(xmlFilePath, "ascii");
-    let parser = xml2js.Parser({ explicitArray: false });
-    let match;
+    const fileData = fs.readFileSync(xmlFilePath, "ascii");
+    const parser = xml2js.Parser({ explicitArray: false });
     parser.parseString(fileData, function (err, result) {
-      result.root.row.find((element) => {
-        if (element.Realty.UniqueId == id) {
-          fs.writeFileSync(jsonFilePath, JSON.stringify(element, null, 2));
-          return;
-        }
+      const match = result.root.row.find((element) => {
+        return element.Realty.UniqueId == id ? element : false;
       });
+      if (match) {
+        fs.writeFileSync(jsonFilePath, JSON.stringify(match, null, 2));
+      } else {
+        console.log("Id doesn't exist");
+        process.exit(1);
+      }
     });
   } catch (error) {
     console.log(`Error converting XML to JSON: ${error}`);
